cdfbb894036c7bc78e6987b309eb1473057a0f3b
Eric W. Greene <eric@ericwgreene.com>
2017-02-21 13:52:15 -0500

    added code demos

--- src/js/app.js
+++ src/js/app.js
@@ -4,17 +4,19 @@ import ReactDOM from 'react-dom';
 class ColorTool extends React.Component {
 
     render() {
-        //return React.createElement('h1', null, 'Color Tool');
+
         return <div>
             <h1>Color Tool</h1>
             <ul>
-                <li>red</li>
-                <li>blue</li>
+                {this.props.colorList.map(color =>
+                    <li>{color}</li>)}
             </ul>
         </div>;
     }
 
 }
 
-ReactDOM.render(<ColorTool />, document.querySelector('main'));
+const colors = [ 'red', 'white', 'blue', 'green', 'saffron' ];
+
+ReactDOM.render(<ColorTool colorList={colors} />, document.querySelector('main'));
 
